919e7021fb9e2753fa49976aeddd05dc8b7279b1
Joonas Kangaskoski <72375590+joonaska@users.noreply.github.com>
2023-07-17 14:18:01 +0300

    Initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+import Heading from "./Heading";
+
+function App() {
+  return (
+    /* voisi olla ilman diviä, kun vaan yksi komponentti */
+    <div>
+      <Heading />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import App from "./components/App";
+/* index pysyy siistinä, kun jaetaan tavarat komponentteihin */
+ReactDOM.render(<App />, document.getElementById("root"));
